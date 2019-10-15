1191b501ae490328379f39f97239047dc1f7bd03
vijay <vijay38576@gmail.com>
2019-10-15 00:04:24 -0500

    Ch2 programs

---
+++ ch2/Exercise4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int value1, value2, diff;
+    value1 = 15;
+    value2 = 87;
+    diff= value2-value1;
+    printf("Difference between %i and %i is %i", value1, value2, diff);
+
+}
